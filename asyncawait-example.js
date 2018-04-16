6f628cdad53647851a945f784c803f5a87382523
tabby42 <irene.theiss@gmx.at>
2018-04-16 20:07:39 +0200

    update files

---
+++ asyncawait-example.js
@@ -0,0 +1,35 @@
+//GET
+
+async function getData() {
+  try {
+    let response = await fetch('https://api-to-call.com/endpoint');
+    if(response.ok) {
+      let jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error('Request failed!');
+  } catch(error) {
+    console.log(error);
+	}
+}
+
+//POST
+
+async function getData() {
+  try {
+    let response = await fetch(
+      'https://api-to-call.com/endpoint',
+      {
+        method: 'POST',
+        body: JSON.stringify({id: 200})
+      }
+    );
+    if (response.ok) {
+      let jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error('Request failed!');
+  } catch(error) {
+    console.log(error);
+  }
+}
---
+++ fetch.js
@@ -0,0 +1,38 @@
+//fetch GET
+
+fetch('https://api-to-call.com/endpoint')
+.then(
+  response => {
+    if(response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  },
+  networkError => {
+    console.log(networkError.message);
+  } 
+)
+.then(
+  jsonResponse => jsonResponse
+);
+
+//fetch POST
+
+fetch('https://api-to-call.com/endpoint',
+      {
+        method: 'POST',
+        body: JSON.stringify({id: '200'})  
+      }
+)
+.then(
+  response => {
+    if(response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  },
+  networkError => console.log(networkError.message)
+)
+.then(
+  jsonResponse => jsonResponse
+);
